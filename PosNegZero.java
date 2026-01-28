491864ae01fd5ac4a472de6671e86bd034b7de7a
mayahthenoob <sherapower2022@gmail.com>
2026-01-28 15:32:20 -0400

    Create PosNegZero.java

---
+++ PosNegZero.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class PosNegZero {
+  public static void main(String[] args) {
+
+    scanner pnz = new scanner;
+
+    if (number > 0) {
+      System.out.println("The number is positive.");
+      s
+    }
+
+    else if (number < 0) {
+      System.out.println("The number is negative.");
+    }
+    
+    else {
+      System.out.println("The number is 0.");
+    }
+  }
+}
